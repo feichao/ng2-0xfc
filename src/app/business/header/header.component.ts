33508f988dfe9b66113c8fd02e534ee86bdd5db1
feichao <len.may@foxmail.com>
2017-06-18 17:07:34 +0800

    mobile art page

--- src/app/business/header/header.component.ts
+++ src/app/business/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  outputs: ['onSelectChange']
 })
 
 export default class HeaderComponent {
+  public selectedRouter: string;
 
+  constructor(private router: Router) {
+    router.events.subscribe((urlObj: any) => this.selectedRouter = urlObj.url);
+  }
+
+  onSelectChange() {
+    this.router.navigateByUrl(this.selectedRouter);
+  }
 }
